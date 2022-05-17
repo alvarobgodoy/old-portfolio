09310c3e32e9fd944d6e8b1c8322524f38f8cc01
alvarobgodoy <alvaro.godoy2002@gmail.com>
2022-05-17 19:24:27 -0300

    add: hamburger menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function toggleSlideMenu() {
+    document.getElementById('menu').classList.toggle('open');
+    document.getElementById('openBtn').classList.toggle('open');
+    document.getElementById('closeBtn').classList.toggle('open');
+}
